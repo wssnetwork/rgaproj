eaee533397e7abc39ada6d63451f7a1db6df5360
wssnetwork <wssnetwork@gmail.com>
2023-06-05 17:32:14 +0800

    add app lb

---
+++ applb.tf
@@ -0,0 +1,49 @@
+resource "aws_lb_target_group" "weblbg-front" {
+    name     = "RGAProj-weblbg-front"
+    port     = 80
+    protocol = "HTTP"
+    vpc_id   = aws_vpc.vpc.id
+    health_check {
+        enabled             = true
+        healthy_threshold   = 3
+        interval            = 10
+        matcher             = 200
+        path                = "/"
+        port                = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = 3
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_target_group_attachment" "weblbg-attach" {
+    count            = length(aws_instance.web)
+    target_group_arn = aws_lb_target_group.weblbg-front.arn
+    target_id        = element(aws_instance.web.*.id, count.index)
+    port             = 80
+}
+
+resource "aws_lb" "weblb-front" {
+    name               = "RGAProj-weblb-front"
+    internal           = false
+    load_balancer_type = "application"
+    security_groups    = [aws_security_group.http-sg.id]
+    subnets            = [for subnet in aws_subnet.vpc-private-subnet : subnet.id]
+
+    enable_deletion_protection = false
+
+    tags = {
+        Environment = "front"
+    }
+}
+
+resource "aws_lb_listener" "weblb-front-end" {
+    load_balancer_arn = aws_lb.weblb-front.arn
+    port              = "80"
+    protocol          = "HTTP"
+
+    default_action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.weblbg-front.arn
+    }
+}
