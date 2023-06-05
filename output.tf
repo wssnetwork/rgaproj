862b99d4932237efadb6ae961f45f397ee3d04d2
wssnetwork <wssnetwork@gmail.com>
2023-06-05 16:21:48 +0800

    fix terraform output

--- output.tf
+++ output.tf
@@ -1,21 +1,12 @@
 # For web-1
-output "public_ip_web_1" {
-    value       = aws_instance.web-1.public_ip
-    description = "Public IP Address of EC2 instance HA 1"
+output "public_ip_web" {
+    value = aws_instance.web.*.public_ip
 }
 
-output "instance_id_web_1" {
-    value       = aws_instance.web-1.id
-    description = "Instance ID HA 1"
+output "instance_id_web" {
+    value = aws_instance.web.*.id
 }
 
-# For web-2
-output "public_ip_web_2" {
-    value       = aws_instance.web-2.public_ip
-    description = "Public IP Address of EC2 instance HA 2"
-}
-
-output "instance_id_web_2" {
-    value       = aws_instance.web-2.id
-    description = "Instance ID HA 2"
+output "instance_web_public_dns" {
+    value = aws_instance.web.*.public_dns
 }
