5c3cdd981b64ffd8730523447d805539105b732b
wssnetwork <wssnetwork@gmail.com>
2023-06-05 16:27:37 +0800

    fix typo in vars

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Create VPC
 resource "aws_vpc" "vpc" {
-    cidr_block              = var.vpc-cidr
+    cidr_block              = var.vpc_cidr
     enable_dns_support      = true
     enable_dns_hostnames    = true
 
